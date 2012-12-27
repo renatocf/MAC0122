2afb0932609660b298c3ddb32890f75d5d9f72bd
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2012-12-27 17:44:10 -0200

    Adding my first program in MAC0122: echo.c (a simple program that echoes what is passed in the command line for it)

---
+++ echo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for(i = 1; i < argc-1; i++)
+	{
+		printf("%s%s", argv[i], " ");
+	}
+	printf("%s%s", argv[argc-1], "\n");
+	
+	return 0;
+}
