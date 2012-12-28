df8a708cdc0afc4a49b71634a0fa781d12d490ed
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2012-12-27 23:03:40 -0200

    Converting tabs to spaces on MAC0122-EP1 directory Applying :retab vim command.

--- MAC0122-EP1/array.c
+++ MAC0122-EP1/array.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	int array[10] = {-5, -4, -3, -2, -1, 1, 2, 3, 4, 5};
-	int i=0, soma = 0;
-	for(i = 0; i < 10; i++)
-		soma += array[i];
-	printf("%d", soma);
-	
-	return 0;
+    int array[10] = {-5, -4, -3, -2, -1, 1, 2, 3, 4, 5};
+    int i=0, soma = 0;
+    for(i = 0; i < 10; i++)
+        soma += array[i];
+    printf("%d", soma);
+    
+    return 0;
 }
