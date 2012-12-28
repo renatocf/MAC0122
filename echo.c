498f6627fc70051faa5dc14797e006dbb139d347
Renato Cordeiro Ferreira <renatocordeiroferreira@gmail.com>
2012-12-27 22:52:39 -0200

    Changing tabs for spaces on simple .c files Applying :retab vim command in calcula_nota.c, soma.c and echo.c .

--- echo.c
+++ echo.c
@@ -2,12 +2,12 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	for(i = 1; i < argc-1; i++)
-	{
-		printf("%s%s", argv[i], " ");
-	}
-	printf("%s%s", argv[argc-1], "\n");
-	
-	return 0;
+    int i;
+    for(i = 1; i < argc-1; i++)
+    {
+        printf("%s%s", argv[i], " ");
+    }
+    printf("%s%s", argv[argc-1], "\n");
+    
+    return 0;
 }
